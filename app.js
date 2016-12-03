88fe8cac8bf76d47c2fbc7138c30effe3f6be343
Stephanie Gonzales <stephanie.gonzales@usu.edu>
2016-12-03 01:23:32 -0700

    made contact page

--- app.js
+++ app.js
@@ -2,31 +2,40 @@ var express = require('express');
 var app = express();
 var request = require('superagent');
 var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var sendmail = require('sendmail')();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
 app.use(bodyParser.json());
 
 // index page
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
   res.render('index');
+
 });
 
-app.post('/results', function(req, res) {
-  var username = req.body.username;
-  request
-    .get('https://api.github.com/users/'+username)
-      .set('Accept', 'application/json')
-      .end(function(err, result){
-        // Calling the end function will send the request
-        res.render('results', { username: result.body.login || '', image: result.body });
-      });
+app.get('/sent', function(req, res, next) {
+    res.render('sent');
+    sendmail({
+        from: req.body.name,
+        to: 'stephanie.gonzales@usu.edu',
+        subject: req.body.email,
+        html: req.body.message,
+    }, function(err, reply) {
+        if(err){
+            res.render('error');
+        }
+        console.log(err && err.stack);
+        console.dir(reply);
+    });
 });
+
 
 app.listen(3000);
 console.log('3000 is the magic port');
